9703693e4ef27fc0a7444aa946de053a895220bb
Emily Kubota <emilykubota@users.noreply.github.com>
2017-03-29 19:46:45 -0700

    Two new subjects

--- analysis/getMeans.m
+++ analysis/getMeans.m
@@ -1,9 +1,12 @@
+% Read in file  
+T = readtable('output.txt','Delimiter', ',', 'Format', '%.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f');
+
 % Create an empty cell and put mean values in it
 C = cell(1,12);
 C(1) = {round(mean(T.RT00),3)};
 C(2) = {round(mean(T.RT01),3)};
 C(3) = {round(mean(T.RT10),3)};
-C(4) = {round(mean(T.RT11),3)}:
+C(4) = {round(mean(T.RT11),3)};
 C(5) = {round(mean(T.RT20),3)};
 C(6) = {round(mean(T.RT21),3)};
 C(7) = {round(mean(T.Acc00),3)};
@@ -14,5 +17,5 @@ C(11) = {round(mean(T.Acc20),3)};
 C(12) = {round(mean(T.Acc21),3)};
 
 % Put data into a table.
-T = cell2table(foo, 'VariableNames', {'RT00', 'RT01', 'RT10', 'RT11', 'RT20'...
+T2 = cell2table(C, 'VariableNames', {'RT00', 'RT01', 'RT10', 'RT11', 'RT20'...
     'RT21', 'Acc00', 'Acc01', 'Acc10', 'Acc11', 'Acc20', 'Acc21'});
---
+++ analysis/mainEffect_nBack.m
@@ -0,0 +1,2 @@
+% Read in file  
+T = readtable('output.txt','Delimiter', ',', 'Format', '%.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f');

53ec298ccc58d7804c28828ffc72fd5bbf3e08eb
Emily Kubota <emilykubota@users.noreply.github.com>
2017-03-30 22:13:51 -0700

    new subject and new analysis scripts

--- analysis/getMeans.m
+++ analysis/getMeans.m
@@ -19,3 +19,13 @@ C(12) = {round(mean(T.Acc21),3)};
 % Put data into a table.
 T2 = cell2table(C, 'VariableNames', {'RT00', 'RT01', 'RT10', 'RT11', 'RT20'...
     'RT21', 'Acc00', 'Acc01', 'Acc10', 'Acc11', 'Acc20', 'Acc21'});
+
+% Get values for intact to plot
+
+intactRT = [T2.RT00(1) T2.RT10(1) T2.RT20(1)];
+degradedRT = [T2.RT01(1) T2.RT11(1) T2.RT21(1)];
+
+%Get acc values to plot
+
+intactAcc = [T2.Acc00(1) T2.Acc10(1) T2.Acc20(1)];
+degradedAcc = [T2.Acc01(1) T2.Acc11(1) T2.RT21(1)];

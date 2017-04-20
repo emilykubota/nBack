7d3252ad44cf75f1fa3d2c4431376eb6d6df8501
Emily Kubota <emilykubota@users.noreply.github.com>
2017-04-19 22:04:35 -0700

    new figures

--- analysis/plot_Acc.m
+++ analysis/plot_Acc.m
@@ -6,19 +6,22 @@ figure; hold('on')
 x = 1:1:2;
 
 % Get the mean RTs and round, and multiply by 100 to turn into percentage.
-y0 = [round(mean(T.Acc00*100),3) round(mean(T.Acc01*100),3)]; 
-err = [(std(T.Acc00*100)/sqrt(24)) (std(T.Acc01*100)/sqrt(24))];
-p1 = errorbar(x, y0, err, 's', 'MarkerSize',12, 'MarkerEdgeColor',[0,.5,.8], 'MarkerFaceColor',[0,.5,.8]);
+Acc_0back = [T.Acc00 T.Acc01];
+y0 = mean(Acc_0back)*100; 
+err = RepMeasErr(Acc_0back)*100;
+p1 = errorbar(x, y0, err, '-s', 'MarkerSize',12, 'MarkerEdgeColor',[0,.5,.8], 'MarkerFaceColor',[0,.5,.8]);
 set(p1,'Color',[0,.5,.8],'LineWidth', 2);
 
-y1 = [round(mean(T.Acc10*100),3) round(mean(T.Acc11*100),3)]; 
-err = [(std(T.Acc10*100)/sqrt(24)) (std(T.Acc11*100)/sqrt(24))];
-p2 = errorbar(x, y1, err, 's', 'MarkerSize',12, 'MarkerEdgeColor',[.5,0,.5], 'MarkerFaceColor',[.5,0,.5]);
+Acc_1back = [T.Acc10 T.Acc11];
+y1 = mean(Acc_1back)*100; 
+err = RepMeasErr(Acc_1back)*100;
+p2 = errorbar(x, y1, err, '-s', 'MarkerSize',12, 'MarkerEdgeColor',[.5,0,.5], 'MarkerFaceColor',[.5,0,.5]);
 set(p2,'Color',[.5,0,.5],'LineWidth', 2);
 
-y2 = [round(mean(T.Acc20*100),3) round(mean(T.Acc21*100),3)]; 
-err = [(std(T.Acc10*100)/sqrt(24)) (std(T.Acc11*100)/sqrt(24))];
-p3 = errorbar(x, y2, err, 's', 'MarkerSize',12, 'MarkerEdgeColor',[.5,0,.5], 'MarkerFaceColor',[.5,0,.5]);
+Acc_2back = [T.Acc20 T.Acc21];
+y2 = mean(Acc_2back)*100; 
+err = RepMeasErr(Acc_2back)*100;
+p3 = errorbar(x, y2, err, '-s', 'MarkerSize',12, 'MarkerEdgeColor',[.5,0,.5], 'MarkerFaceColor',[.5,0,.5]);
 set(p3,'Color',[.5,0,.5],'LineWidth', 2);
 
 
@@ -26,7 +29,7 @@ set(p3,'Color',[.5,0,.5],'LineWidth', 2);
 legend('0-back','1-back','2-back')
 set(gca, 'xtick', 1:2,...
     'xticklabels',{'Intact','50% degraded'})
-xlabel('Task')
+xlabel('Degradation')
 ylabel('Accuracy (% correct)')
 
 return
